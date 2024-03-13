de9dfb524246731cba6efaa0f1f25148aefe8077
Limbodancer23 <83474450+Limbodancer23@users.noreply.github.com>
2024-03-13 22:56:58 +0500

    Добавьте файлы проекта.

---
+++ HW2.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Helpers.h"
+using namespace std;
+int main()
+{
+    std::cout << quater(2, 3);
+}
+
---
+++ Helpers.h
@@ -0,0 +1,4 @@
+#pragma once
+int quater(int a, int b) {
+	return ((a + b) * (a + b));
+}
